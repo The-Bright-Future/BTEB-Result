25234e6330f8f7cf307c4edf6aba0dd4d04d256b
farhan-hoque <fhrubik98@gmail.com>
2023-06-12 23:38:48 +0600

    seperate filed to upload curriculam pdf

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -5,12 +5,13 @@ import axios from 'axios';
 const Home = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [result, setResult] = useState(null);
+  const [curriculam, setCurriculam] = useState(null);
 
   const handleFileChange = (event) => {
     setSelectedFile(event.target.files[0]);
   };
 
-  const handleUpload = async () => {
+  const handleResultUpload = async () => {
     try {
       if (!selectedFile) {
         console.error('No file selected');
@@ -32,14 +33,52 @@ const Home = () => {
     }
   };
 
+  const handleCurriculamUpload = async () => {
+    try {
+      if (!selectedFile) {
+        console.error('No file selected');
+        return;
+      }
+      const formData = new FormData();
+      formData.append('pdfFile', selectedFile);
+
+      const response = await axios.post('http://localhost:7070/api/v1/curriculam-upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      console.log(response.data);
+      setResult(response.data);
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
   return (
     <section>
       <div className='resultUpload'>
+        <h1 className='uploadTitle'>Upload the <br /> result pdf</h1>
         <input type='file' onChange={handleFileChange} className='border-2 border-gray-300 p-2 rounded-lg w-1/2' />
-       
         {
           !result ? <>
-          <button onClick={handleUpload} disabled={!selectedFile} className='border p-6 bg-black fileUploadBtn cursor-pointer'>
+          <button onClick={handleResultUpload} disabled={!selectedFile} className='border p-6 bg-black fileUploadBtn cursor-pointer'>
+          Upload
+        </button>
+          </> : <>
+          <div className='result'>
+            <h1 className='text-2xl font-bold'>Uploaded</h1>
+          </div>
+          </>
+        }
+      </div>
+
+      <div className='resultUpload'>
+        <h1 className='uploadTitle'>Upload the curriculam pdf</h1>
+        <input type='file' onChange={handleFileChange} className='border-2 border-gray-300 p-2 rounded-lg w-1/2' />
+        {
+          !result ? <>
+          <button onClick={handleCurriculamUpload} disabled={!selectedFile} className='border p-6 bg-black fileUploadBtn cursor-pointer'>
           Upload
         </button>
           </> : <>
